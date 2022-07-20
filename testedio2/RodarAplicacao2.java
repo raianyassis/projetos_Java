ca20034510fbf5cb4246a15931c53486402def57
raianyassis <ray.assis00@gmail.com>
2022-07-19 22:43:02 -0300

    Polimorfismo

---
+++ testedio2/RodarAplicacao2.java
@@ -0,0 +1,25 @@
+package testedio2;
+
+public class RodarAplicacao2 {
+
+	public static void main(String[] args) {
+		
+		ClasseMae[] classes = new ClasseMae[] {new ClasseFilha1(), new ClasseFilha2(), new ClasseMae()};
+		
+		for (ClasseMae classe: classes) {
+			classe.metodo1();
+		}
+		
+		System.out.println("");
+		
+		for (ClasseMae classe: classes) {
+			classe.metodo2();
+		}
+		
+		System.out.println("");
+		
+		ClasseFilha2 ClasseFilha2 = new ClasseFilha2();
+		ClasseFilha2.metodo2();
+	}
+
+}
